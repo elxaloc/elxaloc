24b470c3ed5ed5f2963547dd886de902dc415cfc
elxaloc <121422209+elxaloc@users.noreply.github.com>
2023-01-31 04:58:05 -0500

    Update gc-tagall.js

--- plugins/gc-tagall.js
+++ plugins/gc-tagall.js
@@ -13,7 +13,7 @@ conn.sendMessage(m.chat, { text: teks, mentions: participants.map(a => a.id) }, 
 }
 handler.help = ['tagall <mesaje>','invocar <mesaje>']
 handler.tags = ['group']
-handler.command = /^(moños|moñas)$/i
+handler.command = /^(tagall|manca|manco)$/i
 handler.admin = true
 handler.group = true
 export default handler
