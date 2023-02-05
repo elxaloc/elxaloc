01946da522dac14d799d79210c62ec81876c3ed6
elxaloc <121422209+elxaloc@users.noreply.github.com>
2023-02-05 08:47:07 -0500

    Update gc-tagall.js

--- plugins/gc-tagall.js
+++ plugins/gc-tagall.js
@@ -8,7 +8,7 @@ let oi = `*𝙼𝙴𝙽𝚂𝙰𝙹𝙴:* ${pesan}`
 let teks = `*⺀𝙸𝙽𝚅𝙾𝙲𝙰𝙽𝙳𝙾 - 𝙶𝚁𝚄𝙿𝙾⺀*\n\n*VENTA DEL 🤖 wa.me/+51904841810*\n\n❏ ${oi}\n\n❏ *𝙴𝚃𝙸𝚀𝚄𝙴𝚃𝙰𝚂*\n`
 for (let mem of participants) {
 teks += `┣➥ @${mem.id.split('@')[0]}\n`}
-teks += `*└*  IG: @COMUNIDAD.DKN  ᷦ\n\n*▌│█║▌║▌║║▌║▌║▌║█*`
+teks += `*└* *IG: @COMUNIDAD.DKN* \n\n*▌│█║▌║▌║║▌║▌║▌║█*`
 conn.sendMessage(m.chat, { text: teks, mentions: participants.map(a => a.id) }, )
 }
 handler.help = ['tagall <mesaje>','invocar <mesaje>']
